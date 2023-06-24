a72b8deb41d9f7ba193f922a25f96c3214470cec
SHARATKUMAR BHAT <sharatsbhat232@gmail.com>
2023-06-24 11:20:05 +0530

    login working

--- app/src/main/java/com/example/loginpage/Loginpage.java
+++ app/src/main/java/com/example/loginpage/Loginpage.java
@@ -1,6 +1,6 @@
 package com.example.loginpage;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Loginpage extends AppCompatActivity {
@@ -8,7 +8,7 @@ public class Loginpage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginpage);
+        setContentView(R.layout.sign_in);
 
     }
 }
--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.loginpage;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.startup);
     }
 }
